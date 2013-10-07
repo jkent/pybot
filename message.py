0902267c945f980c82cab0645be3a3d6d08f8391
Jeff Kent <jeff@jkent.net>
2013-10-07 14:20:33 -0500

    split parsing into separate functions, add datetime object and default level 0

--- message.py
+++ message.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4 et
 
+from datetime import datetime
 import re
 
 
@@ -31,42 +32,46 @@ prefix_re =  re.compile(
   ')$'
 )
 
+
+def parse_prefix(prefix):
+    match = prefix_re.match(prefix)
+    if match:
+        return match.groupdict()
+    else:
+        return {'nick': None, 'user': None, 'host': None, 'server': None}
+
+def parse_message(message):
+    match = message_re.match(message)
+    if match:
+        d = match.groupdict()
+    else:
+        d = {'prefix': None, 'cmd': None, 'param': None, 'trailing': None}
+
+    if d['prefix']:
+        d.update(parse_prefix(d['prefix']))
+
+    d['cmd'] = d['cmd'].upper()
+
+    d['param'] = d['param'].split() if d['param'] else []
+
+    if d['trailing']:
+        d['param'].append(d['trailing'])
+        d['trailing'] = True
+    else:
+        d['trailing'] = False
+
+    return d
+
+
 class Message(object):
     def __init__(self, line, bot=None):
         self.bot = bot
         self.raw = line
-        self.prefix = None
-        self.cmd = None
-        self.param = None
-        self.trailing = None
-        self.server = None
-        self.nick = None
-        self.user = None
-        self.host = None
-        self.channel = None
         self.reply_to = None
-
-        match = message_re.match(self.raw)
-        if match:
-            self.__dict__.update(match.groupdict())
-
-        if self.prefix:
-            match = prefix_re.match(self.prefix)
-            if match:
-                self.__dict__.update(match.groupdict())
-
-        self.cmd = self.cmd.upper()
-
-        if self.param:
-            self.param = self.param.split()
-        else:
-            self.param = []
-        
-        if self.trailing:
-            self.param.append(self.trailing)
-            self.trailing = True
-        else:
-            self.trailing = False
+        self.time = datetime.utcnow()
+        self.channel = None
+        self.level = 0
+        self.__dict__.update(parse_message(line))
 
         if self.cmd in ['PRIVMSG', 'NOTICE']:
             if self.param[0].startswith(('&', '#', '+', '!')):
